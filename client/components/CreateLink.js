6999c9298c6924b566f03eb634f1048db03b484b
Adithya Kamath <adithyakamath96@gmail.com>
2020-09-17 03:10:53 +0530

    Form input validated with error checks

--- client/components/CreateLink.js
+++ client/components/CreateLink.js
@@ -1,9 +1,23 @@
 import React, { Component } from "react";
 
 class CreateLink extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: "",
+    };
+  }
+
   handleSubmit(event) {
     event.preventDefault();
-    Meteor.call('links.insert',this.refs.input.value);
+    Meteor.call("links.insert", this.refs.input.value, (error) => {
+      if (error) {
+        this.setState({ error: "Enter valid URL" });
+      } else {
+        this.setState({ error: "" });
+        this.refs.input.value = "";
+      }
+    });
   }
 
   render() {
@@ -14,6 +28,7 @@ class CreateLink extends Component {
             <label>Link to shorten</label>
             <input ref="input" className="form-control" />
           </div>
+          <div className="text-danger">{this.state.error}</div>
           <button className="btn btn-primary">Shorten</button>
         </form>
       </div>
--- imports/collections/links.js
+++ imports/collections/links.js
@@ -1,9 +1,20 @@
 import { Mongo } from "meteor/mongo";
+import validUrl from "valid-url";
+import { check, Match } from "meteor/check";
 
 Meteor.methods({
-    'links.insert':function(url){
-        console.log(`Attempting to save ${url}`)
-    }
-})
+  "links.insert": function (url) {
+    // validUrl.isUri(url); - checking if valid URI
+    //check to run validation. if validation passed, nothing is done. If validation failed, error communicated to client
+    check(
+      url,
+      Match.Where((url) => validUrl.isUri(url))
+    ); //Match.where to define custom validation
+
+    //if above check passes, we go down here. if not, it throws an error message
+    const token = Math.random().toString().slice(-5);
+    Links.insert({ url, token, clicks: 0 });
+  },
+});
 
 export const Links = new Mongo.Collection("links");
